7ce865bf7e40337ef9d563c73cec28d817b7a2c5
jaronnie <nstar@gocloudcoder.com>
2021-11-18 19:38:43 +0800

    add docker command of images

---
+++ cmd/docker-images.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+)
+
+func main() {
+	cli, err := client.NewClientWithOpts(client.WithHost("tcp://127.0.0.1:2375"))
+	if err != nil {
+		panic(err)
+	}
+	list, err := cli.ImageList(context.Background(), types.ImageListOptions{})
+	if err != nil {
+		panic(err)
+	}
+	for _, v := range list {
+		fmt.Println(v.RepoTags, v.Created, v.ID)
+	}
+}
