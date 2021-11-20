731f7e335a26174cc5de4b64cad5904d9024dc94
jaronnie <nstar@gocloudcoder.com>
2021-11-20 22:06:37 +0800

    add docker command of inspect

--- cmd/docker-inspect.go
+++ cmd/docker-inspect.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/docker/docker/client"
+	"time"
+)
+
+func main() {
+	cli, err := client.NewClientWithOpts(client.WithHost("tcp://127.0.0.1:2375"))
+	if err != nil {
+		panic(err)
+	}
+	// 3 minutes timeout
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*3)
+	defer cancel()
+	inspect, err := cli.ContainerInspect(ctx, "0d3ddae80cc9f800acb85570371b604acd1867031d70cd9ae3a8ba0530019fd4")
+	if err != nil {
+		panic(err)
+	}
+	marshal, err := json.Marshal(inspect)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(marshal))
+}
