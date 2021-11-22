5032533a56d3a820c275d075e49a02c7f7d658cd
jaronnie <nstar@gocloudcoder.com>
2021-11-22 14:01:32 +0800

    add docker command of top

--- cmd/docker-top.go
+++ cmd/docker-top.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/docker/docker/client"
+	"time"
+)
+
+func main() {
+	cli, err := client.NewClientWithOpts(client.WithHost("tcp://127.0.0.1:2375"))
+	if err != nil {
+		panic(err)
+	}
+	for {
+		top, err := cli.ContainerTop(context.Background(), "308a0bcdebc3", []string{})
+		if err != nil {
+			panic(err)
+		}
+		time.Sleep(time.Second*3)
+		fmt.Println(top)
+	}
+}
