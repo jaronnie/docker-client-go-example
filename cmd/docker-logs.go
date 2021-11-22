5a333f319b1d86b9133fc0f94d122fd707250206
jaronnie <nstar@gocloudcoder.com>
2021-11-22 10:58:33 +0800

    add docker command of logs

--- cmd/docker-logs.go
+++ cmd/docker-logs.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"io"
+	"os"
+	"time"
+)
+
+func main() {
+	cli, err := client.NewClientWithOpts(client.WithHost("tcp://127.0.0.1:2375"))
+	if err != nil {
+		panic(err)
+	}
+	// 10 seconds timeout
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+	logs, err := cli.ContainerLogs(ctx, "ha219aa123", types.ContainerLogsOptions{
+		ShowStdout: true,
+		ShowStderr: true,
+		Follow: true,
+	})
+	defer logs.Close()
+	if err != nil {
+		return
+	}
+	writer, err := io.Copy(os.Stdout,logs)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(writer)
+}
