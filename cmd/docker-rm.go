dd948f30d1b77a73d699c593046da4b4ac1a192e
jaronnie <nstar@gocloudcoder.com>
2021-11-22 15:08:10 +0800

    add docker command of rm

--- cmd/docker-rm.go
+++ cmd/docker-rm.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"context"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"time"
+)
+
+func main() {
+	cli, err := client.NewClientWithOpts(client.WithHost("tcp://127.0.0.1:2375"))
+	if err != nil {
+		panic(err)
+	}
+	// 10 seconds timeout
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+	err = cli.ContainerRemove(ctx, "308a0bcdebc3", types.ContainerRemoveOptions{Force: true})
+	if err != nil {
+		panic(err)
+	}
+}
