c6e99ca9cff168bed66a64446f196180aebf24c4
jaronnie <nstar@gocloudcoder.com>
2021-11-22 15:07:57 +0800

    add docker command of restart

--- cmd/docker-restart.go
+++ cmd/docker-restart.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"context"
+	"github.com/docker/docker/client"
+	"time"
+)
+
+func main() {
+	cli, err := client.NewClientWithOpts(client.WithHost("tcp://127.0.0.1:2375"))
+	if err != nil {
+		panic(err)
+	}
+	// 10 seconds timeout
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+	timeout := time.Second*10
+	err = cli.ContainerRestart(ctx, "308a0bcdebc3", &timeout)
+	if err != nil {
+		panic(err)
+	}
+}
