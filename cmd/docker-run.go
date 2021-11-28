10422f27718de21862f1607827cfb81997901235
jaronnie <nstar@gocloudcoder.com>
2021-11-29 00:19:36 +0800

    upgrade run command

--- cmd/docker-run.go
+++ cmd/docker-run.go
@@ -2,24 +2,21 @@ package cmd
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 	"github.com/docker/go-connections/nat"
-	"io"
 	"math/rand"
-	"os"
+	"mydocker/util"
 	"time"
 )
 
 var DockerYAML = `
-name: host-agent
+hostname: host-agent
 image: gocloudcoder/kube-image-pull:develop
 resources:
-  request:
-    cpu: "100m"
-    memory: "100Mi"
   limits:
     cpu: "100m"
     memory: "500Mi"
@@ -40,57 +37,85 @@ resources:
       value: Logger
     - name: LoggerWS
       value: LoggerWS 
-ports:
-  - containerPort: 9009
-    protocol: tcp  
+  ports:
+    - name: agent
+      containerPort: 8888
+      protocol: tcp  
 `
 
-//type Config struct {
-//	Resources
-//
-//	Name string
-//	Image string
-//
-//}
+type Config struct {
+	Resources `json:"resources"`
 
-//type Resources struct {}
+	Hostname string `json:"hostname"`
+	Image string `json:"image"`
+}
 
+type Resources struct {
+	Limits 	`json:"limits"`
+	Envs []Env `json:"env"`
+	Ports []Port `json:"ports"`
+}
+
+type Limits struct {
+	Cpu string `json:"cpu"`
+	Memory string `json:"memory"`
+}
+
+type Env struct {
+	Name string `json:"name"`
+	Value string `json:"value"`
+}
+
+type Port struct {
+	Name string `json:"name"`
+	ContainerPort int `json:"containerPort"`
+	Protocol string `json:"protocol"`
+}
 
 func Run() {
+	// generate client
 	cli, err := client.NewClientWithOpts(client.WithHost("tcp://127.0.0.1:2375"))
 	if err != nil {
 		panic(err)
 	}
+	
 	// pull image
 	// 3 minutes timeout
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*3)
 	defer cancel()
-	reader, err := cli.ImagePull(ctx, "gocloudcoder/kube-image-pull:develop", types.ImagePullOptions{})
+	_, err = cli.ImagePull(ctx, "gocloudcoder/kube-image-pull:develop", types.ImagePullOptions{})
 	if err != nil {
 		panic(err)
 	}
-	_, err = io.Copy(os.Stdout,reader)
+	
+	// get config from yaml
+	content, err := util.Yaml2Json([]byte(DockerYAML))
 	if err != nil {
 		panic(err)
 	}
+	
+	// unmarshal config
+	var config Config
+	err = json.Unmarshal(content, &config)
+	if err != nil {
+		panic(err)
+	}
+
+	// generate envs
+	envs := SetEnv(config.Envs)
+
+	// generate portBindings
+	bindings := SetPortBindings(config.Ports)
+
 	// create container
-	// portMap
-	portMap := make(map[nat.Port][]nat.PortBinding)
-	// bind tcp/8888(container) -> 0.0.0.0:28888(host)
-	portMap["8888/tcp"] = []nat.PortBinding{
-		{
-			HostIP:   "0.0.0.0",
-			HostPort: "0", // 随机分配
-		},
-	}
 	createResp, err := cli.ContainerCreate(ctx, &container.Config{
-		Hostname: "blocface-hostagent",
-		Image:    "gocloudcoder/kube-image-pull:develop",
+		Hostname: config.Hostname,
+		Image:    config.Image,
 		// set env
-		Env: []string{"machineID=1"},
+		Env: envs,
 	}, &container.HostConfig{
 		// bind port
-		PortBindings: portMap,
+		PortBindings: bindings,
 		// limit cpu and memory
 		Resources: container.Resources{
 			Memory: 104857600, // 100M
@@ -99,7 +124,7 @@ func Run() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(createResp.ID)
+
 	// start container
 	err = cli.ContainerStart(ctx, createResp.ID, types.ContainerStartOptions{})
 	if err != nil {
@@ -114,4 +139,26 @@ func generateRandomHostname() string {
 	t := fmt.Sprintf("%d", time.Now().UnixNano())
 	f := func() int64 { return rand.Int63n(9999-1000) + 1000 }
 	return fmt.Sprintf("docker-%s-%d-%d", t[len(t)-4:], f(), f())
+}
+
+func SetEnv(envs []Env) []string {
+	setEnvs := make([]string, 0)
+	for _, v := range envs {
+		setEnvs = append(setEnvs, fmt.Sprintf("%s=%s", v.Name, v.Value))
+	}
+	return setEnvs
+}
+
+func SetPortBindings(ports []Port) map[nat.Port][]nat.PortBinding {
+	// portMap
+	portMap := make(map[nat.Port][]nat.PortBinding)
+	for _, v := range ports {
+		portMap[nat.Port(fmt.Sprintf("%d/%s", v.ContainerPort, v.Protocol))] = []nat.PortBinding {
+			{
+				HostIP:   "0.0.0.0",
+				HostPort: "0", // 随机分配
+			},
+		}
+	}
+	return portMap
 }
--- util/util.go
+++ util/util.go
@@ -1,7 +1,19 @@
 package util
 
+import (
+	yaml2 "k8s.io/apimachinery/pkg/util/yaml"
+)
+
 func GetContainerIDByMachineUUID(uuid string) (string, error) {
 	return "", nil
+}
+
+func Yaml2Json(content []byte) ([]byte, error) {
+	data, err := yaml2.ToJSON(content)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
 }
 
 
